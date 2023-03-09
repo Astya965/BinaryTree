667f8a9800c1b90307543734fe85837325f1b9b7
astya965 <astya965@gmail.com>
2023-03-09 12:59:26 +0100

    feat: Add Node class and functions

---
+++ src/types/TreeNode.ts
@@ -0,0 +1,33 @@
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, parent?: TreeNode | null, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export const insertNode = (tree: TreeNode | null, value: number) => {
+  let node: TreeNode | null = tree;
+  let key: "left" | "right" | null = null;
+
+  while (node && value !== node.val) {
+    key = value < node.val ? "left" : "right";
+    if (!node[key]) {
+      node[key] = new TreeNode(value);
+      break;
+    }
+    node = node[key];
+  }
+
+  return tree;
+};
+
+export const createTreeFromArray = (array: number[]) =>
+  array.reduce(
+    (acc: TreeNode | null, cur) =>
+      acc ? insertNode(acc, cur) : new TreeNode(cur),
+    null
+  );
